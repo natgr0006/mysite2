534dd2cbe1b551a8ac237e6c68e9cc832d75bdd0
natgr0006 <65819514+natgr0006@users.noreply.github.com>
2020-05-30 23:37:33 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		nextArrow:'<button class="slick-arrow slick-next"><img src="image/sl-arrow_r.svg" alt=""></button>',
+		prevArrow:'<button class="slick-arrow slick-prev"><img src="image/sl-arrow_l.svg" alt=""></button>'
+	})
+
+});
+
